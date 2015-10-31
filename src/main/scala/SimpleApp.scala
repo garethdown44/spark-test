afb1d691ef3b791c418fac3ad17d045cd8ae1417
Gareth Down <g@garethdown.com>
2015-10-31 11:33:07 +0000

    calculate percentage increase

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -8,10 +8,10 @@ import org.apache.spark.SparkConf
 // 
 
 object Mappers {
-    def toYearsPricesAndPostcodes(s: String) : ( (Int, String), Int) = {
+    def toYearsPricesAndPostcodes(s: String) : ( (Int, String), Double) = {
 
         val splitted = s.split(",")
-        val price = splitted(1).replace("\"", "").toInt
+        val price = splitted(1).replace("\"", "").toDouble
         val year = splitted(2).substring(0, 4).toInt
         val postcodePrefix = splitted(3).replace("\"", "").split(" ")(0)
 
@@ -24,15 +24,11 @@ object Mappers {
     // }
 }
 
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
 object SimpleApp {
   def main(args: Array[String]) {
-    /*val logFile = "file:///d:/dev/spark-1.5.1/README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application")
-    val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))*/
 
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
@@ -40,7 +36,10 @@ object SimpleApp {
     //val postcodes = sc.textFile("file:///d:/codepo_gb/Data/CSV/*.csv")
     //val housePrices = sc.textFile("file:///d:/pp-2015.txt").cache
 
-    val housePrices = sc.textFile("file:///c:/dev/spark/example.txt").cache
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
+
+    val housePrices = sc.textFile("./example2.txt").cache
 
     val prices = housePrices.map(Mappers.toYearsPricesAndPostcodes)
     val count = prices.count
@@ -49,56 +48,61 @@ object SimpleApp {
 
     val averagePrices = totalPricesTotalCounts.mapValues(x => x._1 / x._2)
 
-    //val percentIncreasePerYear = averagePrices.reduce( (a, b) => (a._2 + b._2 )
+    val mapped = averagePrices.map{ case ((y,p), pr) => (p, (y,pr)) } // ((1995, CB11), 100000) --> (CB11, (1995, 100000))
 
-    // ((1995, cb11), 100000)
-    // ((1996, cb11), 200000)
+    println("mapped has these items")
+    mapped.foreach(println)
 
-    // map to cb11, (1995, 100000)
-    //        cb11, (1996, 200000)
+    val firsts = mapped.map{ case (p, (y,pr)) => (p, y % 1995) -> (y, pr)}
+    val seconds = mapped.map{ case (p, (y,pr)) => (p, y % 1996) -> (y, pr)}
+
+    println("firsts")
+    firsts.foreach(println)
+
+    println("seconds")
+    seconds.foreach(println)
+
+    println("joined")
+    val joined = firsts.join(seconds)
+
+    joined.foreach(println)
+
+    val percentIncrease = joined.map{ case ( (p,i), ((y1,pr1), (y2,pr2))) => p -> (y2, (pr1 / pr2)*100) }
+
+    println("percent increase:")
+    percentIncrease.foreach(println)
+
+    // a -> 1, 100
+    // a -> 2, 200
+    // a -> 3, 250
+
+    // b -> 1, 300
+    // b -> 2, 350
+
+    // c -> 1, 400
+
+    // mod the year by either the start year or the next year to get the same indeces
+
+    // map ->
     //
-    // sort by value._1
-    //
-    // reduce to pc, (y, percent)
-    //
-    // map back to (year, postcode), value
+    // (a,0) -> 1,100
+    // and 
+    // (a,0) -> 2,200
 
-    //val percentIncreasePerYear = averagePrices.reduceByKey( (a, b) => a / b)
+    // then join them and map back out
 
-    //percentIncreasePerYear.foreach(println)
+    // (a,2) -> (100,200)
+    // (a,3) -> (200,250)
 
-    val mapped = averagePrices.map{ case ((y,p), pr) => (p, (y,pr))} // ((1995, CB11), 100000) --> (CB11, (1995, 100000))
+    // map values again to get percentage increase
 
-    val drop1ed = mapped.filter { case (k, v) => k > 1 }
 
-    val zipped = mapped.zip(drop1ed)
+    // within each group we want to zip
 
-    zipped.foreach(println)
+    // a -> ( (1, 100), (2, 200) )
+    // a -> ( (2, 200), (3, 250) )
+
 
     sc.stop
-
-    //val averagesPerYear = totalsPerYear.
-
-    //averagePerYear.foreach(println)
-
-    //println("** Average price - %s".format(averagePrice))
-
-    // xs = 1,2,3,4
-    // xs
-    // ys = xs.drop 1
-
-    // xs.zip(ys) = (1, 2), (2, 3), (3, 4)
-
-
-    // 1995, 1
-    // 1996, 2
-    // 1997, 4
-
-    // 1995, (0, 1)
-    // 1996, (1, 2)
-    // 1997, (2, 4)
-
-    // 1995, 0
-    // 1996, 
   }
 }
