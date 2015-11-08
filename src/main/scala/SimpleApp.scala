df73ac64b4c4bf413f5e11aa7dfbc659e2a3f101
Gareth Down <g@garethdown.com>
2015-11-08 23:38:57 +0000

    try to plug into old code

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -42,104 +42,51 @@ object SimpleApp {
 
     val prices = housePrices.map(toYearsPricesAndPostcodes)
 
-    //val mapped = prices.map{ case ( (y, pc), pr) => Array(pc, y, pr).mkString(",") }
-    //mapped.coalesce(1, true).saveAsTextFile("output")
-
-    //prices.mapValues{ case (x: Int, _ : Double) => x.toDouble}.reduceByKey((x, y) => x + y)
-    //val meanOfY = prices.mapValues(x => x._2).reduce((x, y) => x + y) / prices.count
-    // val meanOfXYs = prices.map(x => x._2._1 * x._2._2).reduce((x, y) => x + y) / prices.count
-    // val meanOfXSquared = prices.map(x => Math.pow(x._2._1, 2)).reduce((x, y) => x + y) / prices.count
-
-    // val totalYsTotalCounts = prices.mapValues(x => (x._2, 1)).reduceByKey( (x, y) => (x._1 + y._1, x._2 + y._2) )
-    // val meanOfYs = totalYsTotalCounts.mapValues(x => x._1 / x._2)
-
-    // val totalXsTotalCounts = prices.mapValues(x => (x._1, 1)).reduceByKey( (x, y) => (x._1 + y._1, x._2 + y._2) )
-    // val meanOfXs = totalXsTotalCounts.mapValues(x => x._1 / x._2)
-
-    // val totalXYsTotalCounts = prices.mapValues(x => (x._1 * x._2, 1)).reduceByKey( (x, y) => (x._1 + y._1, x._2 + y._2) )
-    // val meanOfXYs = totalXYsTotalCounts.mapValues(x => x._1 / x._2)
-
-    // val totalXSquaredsTotalCounts = prices.mapValues(x => (Math.pow(x._1, 2), 1)).reduceByKey( (x, y) => (x._1 + y._1, x._2 + y._2) )
-    // val meanOfXSquareds = totalXSquaredsTotalCounts.mapValues(x => x._1 / x._2)
-
     val values = prices.mapValues(x => (x._1, x._2, x._1 * x._2, Math.pow(x._1, 2), 1))
                        .reduceByKey{ case ((xsa, ysa, xysa, x2sa, na), (xsb, ysb, xysb, x2sb, nb)) => ((xsa + xsb), (ysa + ysb), (xysa + xysb), (x2sa + x2sb), (na + nb)) }
                        .mapValues{ case (xs, ys, xys, x2s, n) => ( (xs / n).toDouble, ys / n, xys / n, x2s / n) }
                        .mapValues(calcMandB)
                        .mapValues(calcYs)
                        .flatMapValues(x => x)
-                       .map{ case (pc, (y, pr)) => ((pc, y), pr) }
+                       //.map{ case (pc, (y, pr)) => ((pc,y), pr) }
 
     values.coalesce(1, true).foreach(println)
 
+    values.foreach(println)
 
-    // println("mean of x")
-    // println(meanOfX)
 
-    // println("mean of y")
-    // println(meanOfY)
+    val count = values.count
 
-    // println("mean of xys")
-    // println(f"$meanOfXYs%2.2f")
+    val totalPricesTotalCounts = values.mapValues(x => (x, 1)).reduceByKey( (x, y) => (x._1 + y._1, x._2 + y._2) )
 
-    // println("mean of x squared")
-    // println(meanOfXSquared)
+    val averagePrices = totalPricesTotalCounts.mapValues(x => x._1 / x._2)
 
-    // val m = (( (meanOfX * meanOfY) - meanOfXYs)) / ((Math.pow(meanOfX, 2) - meanOfXSquared).toDouble)
-
-    // println("m")
-    // println(f"$m%2.2f")
-
-    // val b = meanOfY - (m * meanOfX)
-
-    // val y1995 = (m * 1995) + b
-    // println("y 1995")
-    // println(y1995)
-
-    // val calcY = (mp : Double, x : Double, bp : Double) => (mp * x) + bp
-
-    // val range = 1995 until 2015
-
-    // val ys = range.map(x => calcY(m, x, b))
-
-    // ys.foreach(println)
-
-    //val b = prices.reduce( (a, b) => 1 + b._2)
-
-    //println(meanOfX.toString)
-
-    //val count = prices.count
-
-    // val totalPricesTotalCounts = prices.mapValues(x => (x, 1)).reduceByKey( (x, y) => (x._1 + y._1, x._2 + y._2) )
-
-    // val averagePrices = totalPricesTotalCounts.mapValues(x => x._1 / x._2)
-
-    // val mapped = averagePrices.map{ case ((y,p), pr) => (p, (y,pr)) } // ((1995, CB11), 100000) --> (CB11, (1995, 100000))
+    val mapped = averagePrices.map{ case ((y,p), pr) => (p, (y,pr)) } // ((1995, CB11), 100000) --> (CB11, (1995, 100000))
 
     // println("mapped has these items")
 
-    // //mapped.coalesce(1,true).saveAsTextFile("./mapped.txt")
+    //mapped.coalesce(1,true).saveAsTextFile("./mapped.txt")
 
-    // val firsts = mapped.map{ case (p, (y,pr)) => (p, y % 1995) -> (y, pr)}
-    // val seconds = mapped.map{ case (p, (y,pr)) => (p, y % 1996) -> (y, pr)}
+    val firsts = mapped.map{ case (p, (y,pr)) => (p, y % 1995) -> (y, pr)}
+    val seconds = mapped.map{ case (p, (y,pr)) => (p, y % 1996) -> (y, pr)}
 
-    // val joined = firsts.join(seconds)
+    val joined = firsts.join(seconds)
 
-    // //joined.coalesce(1,true).saveAsTextFile("./joined.txt")
+    //joined.coalesce(1,true).saveAsTextFile("./joined.txt")
 
-    // val percentIncrease = joined.map{ case ( (p,i), ((y1,pr1), (y2,pr2))) => p -> (y2, pr1, pr2, calcPercent(pr1, pr2) ) }
+    val percentIncrease = joined.map{ case ( (p,i), ((y1,pr1), (y2,pr2))) => p -> (y2, pr1, pr2, calcPercent(pr1, pr2) ) }
 
-    // // val filtered = percentIncrease.filter{ case (p, (y2, pr1, pr2, percent)) => p == "CB11" && y2 == 2014 }
+    // val filtered = percentIncrease.filter{ case (p, (y2, pr1, pr2, percent)) => p == "CB11" && y2 == 2014 }
 
-    // println("percent increase:")
+    println("percent increase:")
 
-    // //val finalValues = percentIncrease.coalesce(1,true)
+    //val finalValues = percentIncrease.coalesce(1,true)
 
-    // //finalValues.saveAsTextFile("./output.txt")
+    //finalValues.saveAsTextFile("./output.txt")
 
-    // val csvValues = filtered.map{ case (p, (y2, pr1, pr2, percent)) => Array(p, y2, pr1, pr2, percent).mkString(",") }
+    val csvValues = filtered.map{ case (p, (y2, pr1, pr2, percent)) => Array(p, y2, pr1, pr2, percent).mkString(",") }
 
-    // csvValues.coalesce(1, true).saveAsTextFile("output")
+    csvValues.coalesce(1, true).saveAsTextFile("output")
 
     sc.stop
     }
